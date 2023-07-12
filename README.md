85469942ddb09e2a23b5871822dd6eaf1cddbf5d
Jason <none>
2023-07-12 15:33:03 +0800

    add periphery contract

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # uniswap-v3
-状态：开发中
+状态：开发中,periphery的合约还不能使用。目前只有factory能够成功部署
 
 基于hardhat框架管理和发布uniswap v3
 建议结合本文来深入了解源码：
@@ -7,7 +7,10 @@
 
 ## 目录说明
 frontend：前端代码
-haddhat：合约，基于hardhat框架
+hardhat：合约，基于hardhat框架
+hardhat/contracts：其中有具体的solidity合约
+    1. factory：为工厂合约，v3核心业务逻辑均在此处
+    2. periphery：外围合约，其中包括路由合约，nft等等，该外围合约调用factory合约，实现完整的业务逻辑
 
 ## 合约编译
 ```shell
@@ -17,9 +20,15 @@ npx hardhat compile
 
 ## 发布
 ```shell
-# 2. 发布工厂合约
+# 1. 发布工厂合约
 npx hardhat --network goerli run scripts/deploy_1_factory.js 
+
+# 2. 发布路由合约，其中需要传入两个参数：factory合约地址以及weth9合约地址
+# 其中factory合约地址使用第一步返回的地址，weth9合约地址用的是我以前发布的地址（详见下面备注）
+# 当前该合约由于依赖问题，还异常，后续处理
+npx hardhat --network goerli run scripts/deploy_2_router.js 
 ```
+`备注`：weth9合约，可以查阅我以前uniswap-v2项目整合时的实现源码：[uniswap-v2 整合部署和发布](https://github.com/jason-wj/uniswap-v2)
 
 ## 信息整理
 使用本项目，goerli测试网自行发布信息如下：
---
+++ hardhat/scripts/deploy_2_router.js
@@ -0,0 +1,36 @@
+// This is a script for deploying your contracts. You can adapt it to deploy
+// yours, or create new ones.
+
+async function main() {
+    // This is just a convenience check
+    if (network.name === "hardhat") {
+        console.warn(
+            "You are trying to deploy a contract to the Hardhat Network, which" +
+            "gets automatically created and destroyed every time. Use the Hardhat" +
+            " option '--network localhost'"
+        );
+    }
+
+    // ethers is available in the global scope
+    const [deployer] = await ethers.getSigners();
+    console.log(
+        "Deploying the contracts with the account:",
+        await deployer.getAddress()
+    );
+
+    console.log("Account balance:", (await deployer.getBalance()).toString());
+
+    //solidity version：0.7.6
+    const Router = await ethers.getContractFactory("SwapRouter");
+    const router = await Router.deploy('0xC5F57433074986CD739900242033Ca5E5f6da4be','0xFe33eC9960E430608030e92860264B486Ae99Ef2');
+    await router.deployed();
+
+    console.log("Router address:", router.address);
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
